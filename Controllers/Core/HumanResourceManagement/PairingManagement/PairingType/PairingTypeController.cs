c257bd8e685db6deba265a34e2a463e5a91b3bc1
Felipe Machado <felipe.barcellos.machado@gmail.com>
2025-12-17 05:13:49 -0300

    Ajustes

--- Controllers/Core/HumanResourceManagement/PairingManagement/PairingType/PairingTypeController.cs
+++ Controllers/Core/HumanResourceManagement/PairingManagement/PairingType/PairingTypeController.cs
@@ -88,7 +88,7 @@ namespace Workforce.Server.Controllers.Core.HumanResourceManagement.PairingManag
             try
             {
                 // Debug log
-                Console.WriteLine($"PairingTypeController.InsertAsync: Received - Id={entity.Id}, EnvironmentId={entity.EnvironmentId}, Name={entity.Name}, Mandatory={entity.Mandatory}");
+                Console.WriteLine($"PairingTypeController.InsertAsync: Received - Id={entity.Id}, EnvironmentId={entity.EnvironmentId}, Name={entity.Name}, Satisfability={entity.Satisfability}");
 
                 // Validate model
                 if (entity.EnvironmentId <= 0)
